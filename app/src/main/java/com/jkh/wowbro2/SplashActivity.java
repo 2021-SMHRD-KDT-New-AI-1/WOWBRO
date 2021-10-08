6623c593a700f9dfd844f068857547319fe704b4
KyunghoonJeong <90249583+KyunghoonJeong@users.noreply.github.com>
2021-10-08 17:22:11 +0900

    <update> 스플래시 액티비티 사진 비율 수정

---
+++ app/src/main/java/com/jkh/wowbro2/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.jkh.wowbro2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.jkh.wowbro2.TutorialActivity;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+
+    }
+}
