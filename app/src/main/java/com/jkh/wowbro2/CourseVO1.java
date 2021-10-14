0370f4a4a0a8e0c77383eba7dc5d900ba080b976
조현중 <chj113024@gmail.com>
2021-10-14 16:59:03 +0900

    <update> 테스트

--- app/src/main/java/com/jkh/wowbro2/CourseVO1.java
+++ app/src/main/java/com/jkh/wowbro2/CourseVO1.java
@@ -7,12 +7,16 @@ public class CourseVO1 implements Serializable {
     private String imgPath;
     private String name;
     private String location;
+    private String story;
+    private String sub_name;
 
 
-    public CourseVO1(String imgPath, String name, String location){
+    public CourseVO1(String imgPath, String name, String location, String story, String sub_name){
         this.imgPath = imgPath;
         this.name = name;
         this.location = location;
+        this.story = story;
+        this.sub_name = sub_name;
     }
 
     public String getImgPath() {
@@ -25,5 +29,12 @@ public class CourseVO1 implements Serializable {
 
     public String getLocation() {
         return location;
+    }
+
+    public String getStory() {
+        return story;
+    }
+    public String getSub_name() {
+        return sub_name;
     }
 }
--- app/src/main/java/com/jkh/wowbro2/storyActivity.java
+++ app/src/main/java/com/jkh/wowbro2/storyActivity.java
@@ -8,9 +8,11 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+
 public class storyActivity extends AppCompatActivity {
-    TextView travel_name;
-    ImageView story_backimg;
+    TextView travel_name, travel_desc, travel_story;
+    ImageView story_backimg, story_img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +21,16 @@ public class storyActivity extends AppCompatActivity {
 
         travel_name = findViewById(R.id.travel_name);
         story_backimg = findViewById(R.id.story_backimg);
+        travel_desc = findViewById(R.id.travel_desc);
+        travel_story = findViewById(R.id.travel_story);
+        story_img = findViewById(R.id.story_img);
 
         Intent info = getIntent();
         CourseVO1 desInfo = (CourseVO1) info.getSerializableExtra("info");
         travel_name.setText(desInfo.getName());
+        travel_desc.setText(desInfo.getSub_name());
+        travel_story.setText(desInfo.getStory());
+        Glide.with(this).load(desInfo.getImgPath()).into(story_img);
 
         story_backimg.setOnClickListener(new View.OnClickListener() {
             @Override
